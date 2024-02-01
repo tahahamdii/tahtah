5d775ba6bf45d9cd9ca5eeb6e6c9834e8f8180ff
Hamdi Taha <hamdi.taha@esprit.tn>
2024-02-01 17:42:09 +0100

    redeploy2

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -5,7 +5,6 @@ import dynamic from "next/dynamic";
 export const components = {
   biography: dynamic(() => import("./Biography")),
   content_index: dynamic(() => import("./ContentIndex")),
-  educationn: dynamic(() => import("./Educationn")),
   experience: dynamic(() => import("./Experience")),
   fields_list: dynamic(() => import("./FieldsList")),
   hero: dynamic(() => import("./Hero")),
